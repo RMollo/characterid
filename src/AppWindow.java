a5ba553af3560a3e402678f46a3ed58721ab1d17
Robert Mollo <RMollo@deerfield.edu>
2016-02-04 07:54:53 -0500

    attempt to add second JTextfield

--- src/AppWindow.java
+++ src/AppWindow.java
@@ -1,7 +1,7 @@
 
 import java.awt.*;
 import java.applet.*;
-
+import javax.swing.*;
 
 
 public class AppWindow extends Applet {
@@ -12,7 +12,10 @@ public class AppWindow extends Applet {
 		
 		TextField txtCharacter = new TextField("Character", 20);
 		
-	
+		
+		Label lblHex = new Label("Hex");
+		TextField txtHex = new TextField("Hex", -10);
+		String CharChoice = txtCharacter.getText();
 
 		add(lblCharacter);                
 		add(txtCharacter);                
