3359cb4c0892d90803dfb5523a2c141130296c61
Robert Mollo <RMollo@deerfield.edu>
2016-02-16 12:29:54 -0500

    successfully reads entityfacts.csv (console only)

--- src/showfont.java
+++ src/showfont.java
@@ -1,16 +1,58 @@
-import java.awt.GraphicsEnvironment;
+//this .java is accidentally named showfont; 
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.FileNotFoundException;
 
 
-public class showfont
-{
 
-	public static void main ( String [] args )
-	{
-		GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
-	      String[] names = ge.getAvailableFontFamilyNames();
-	      for ( int i=0; i<names.length; i++ )
-	         {
-	             System.out.println( names[i] );
-	         }
+
+public class showfont {
+	
+// trying to read .csv of character ids, but I really have no idea..
+	public static void main ( String [] args) {
+	
+		showfont obj = new showfont();
+		obj.run();
+		
+	}
+	
+	public void run() {
+		
+		String csvFile = "/Users/rmollo/Desktop/entityfacts.csv";
+		BufferedReader br = null;
+		String line = "";
+		String cvsSplitBy = ",";
+		
+		try {
+			
+			br = new BufferedReader(new FileReader(csvFile));
+			while ((line = br.readLine()) != null) {
+
+
+				String[] answer = line.split(cvsSplitBy);
+				if (answer.length < 6) { System.out.println(line ); continue; }
+				System.out.println( answer[1]
+						+answer[2] );
+						
+	}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (br !=null ) {
+				try {
+					br.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		System.out.println("Done");
 	}
 }
+		
+	
+
