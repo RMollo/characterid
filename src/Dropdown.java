2d4a026d3fa3cf291bab0738baae4dcf10cd05d4
Robert Mollo <RMollo@deerfield.edu>
2016-02-23 10:10:20 -0500

    final- broken

--- src/Dropdown.java
+++ src/Dropdown.java
@@ -1,20 +1,22 @@
 import java.awt.GraphicsEnvironment;
 import javax.swing.JOptionPane;
-
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+//a more in-depth attempt at a dropdown
 public class Dropdown{
 	
-	public static void main ( String[] args) {
-		
+	public static void main ( String[] args) throws FileNotFoundException{
+	
 		GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
 	      String[] names = ge.getAvailableFontFamilyNames();
-	      
+      
 	      for ( int i=0; i<names.length; i++ )
-	    	  
-	    	  String input = (String) JOptionPane.showInputDialog(null, "Testing123...", JOptionPane.QUESTION_MESSAGE, null
+	    	      	  String input = (String) JOptionPane.showInputDialog(null, "Testing123...", JOptionPane.QUESTION_MESSAGE, null
 	    			  
-	    			  names,
-	    			  names[1];
-	    	  System.out.println(input);
-	    	  
-	}	
+//	    			  names,
+//	    			  names[1];
+//	    	  System.out.println(input);
+//	    	  
+//	}	
 }
--- src/GUI.java
+++ src/GUI.java
@@ -1,21 +1,63 @@
 
 import java.applet.*;
 import java.awt.*;
+import java.util.Scanner;
+import javax.swing.JButton;
+
 import java.awt.event.*;
+import javax.swing.*;
+
 
 public class GUI extends Applet implements ActionListener{
-	TextField tf = new TextField(20);
+{
+
+	JFrame frame = new JFrame("Window");
+	frame.setVisible(true);
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	frame.setSize(300,300);
+	JPanel panel = new JPanel();
+	JButton button = new JButton("Convert");
+	panel.add(button);
 	
-	public GUI() {
-		add(tf);
-		tf.addActionListener(this);
+	TextField tf = new TextField(20);
+	TextField tf2 = new TextField(20);
+	
+	add(tf);
+	tf.addActionListener(this);
+	add(tf2);
+	tf2.addActionListener(this);
+
+	Label lbltf = new Label("Enter your character");
+	add(lbltf);
+	String s = tf.getText();
+	button.addActionListener(new Action());
+	
+}
+	static class Action implements ActionListener{
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+		
 	}
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		String s = tf.getText();
-		String sUp = s.toUpperCase();
-		tf.setText(sUp);
+		
+		public void actionPerformed (ActionEvent e) {
 		
 		
-	}
+		
+		 char[] chars = s.toCharArray();
+	      StringBuffer hex = new StringBuffer();
+	      for (int i = 0; i < chars.length; i++)
+	      {
+	         hex.append(Integer.toHexString((int) chars[i]));
+	      }
+	      tf2.setText(hex.toString());
+	      
+	      
+	
+	
+		}
+
 }
